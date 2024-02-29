1e52797ccbf6075c02b421b3afc673b9a8e9d957
Paung <paungcuy15@gmail.com>
2024-02-29 21:28:06 +0700

    29 feb 2024

--- app/Http/Controllers/Api/StorageController.php
+++ app/Http/Controllers/Api/StorageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Response;
@@ -29,4 +30,27 @@ class StorageController extends Controller
 
         // return response($file, 200)->header('Content-Type', $type);
     }
+
+    public function getImageBase64(Request $request){
+        if($request->has('filename')){
+            $filename = $request->input('filename');
+            $path = storage_path('app/public/' . $filename);
+
+            if (!File::exists($path)) {
+                abort(404);
+            }
+        
+            $file = File::get($path);
+            $type = File::mimeType($path);
+        
+            $response = Response::make($file, 200);
+            $response->header("Content-Type", $type);
+        
+            return response()->json([
+                'debug' => base64_encode($file)
+                // 'debug' => base64_encode($response)
+            ], 200);
+        }
+    }
+
 }
